cfe37a00d38abe66abdfa24f8a188d05bce69efa
xiaodi <saybabi@163.com>
2018-10-20 09:06:10 +0800

    first commit

---
+++ src/components/renderComponent.vue
@@ -0,0 +1,12 @@
+<script lang="tsx">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class FilterItem extends Vue {
+  render (h, context) {
+    return <div>
+      Hello, Xiaodi
+    </div>
+  }
+}
+</script>
